8c076510ee51c9d8b1586d0e75fa7dad9ecb5000
poly28 <horimasa0328@gmail.com>
2021-05-03 20:50:14 +0900

    #56 Bitcoinの価格表示まとめ

--- js/main.js
+++ js/main.js
@@ -9,10 +9,15 @@ new Vue({
 	},
 	methods: {},
 	mounted: function () {
-		axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(
-			function (response) {
-				this.bpi = response.data.bpi;
-			}.bind(this)
-		);
+		axios
+			.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+			.then(
+				function (response) {
+					this.bpi = response.data.bpi;
+				}.bind(this)
+			)
+			.catch(function (error) {
+				console.log(error);
+			});
 	},
 });
