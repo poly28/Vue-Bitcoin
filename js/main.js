b581fa5e18695d61867b5cd80303dc5c5384d4f0
poly28 <horimasa0328@gmail.com>
2021-05-03 20:34:49 +0900

    Bitcoin価格表示アプリ #54

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@
 
 new Vue({
 	el: '#app',
-	data: {},
+	data: {
+		bpi: null,
+	},
 	methods: {},
+	mounted: function () {
+		axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(
+			function (response) {
+				this.bpi = response.data.bpi;
+			}.bind(this)
+		);
+	},
 });
